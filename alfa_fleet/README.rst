3148cd663ffc5361625be015e8d7f2fd83b7b036
Dorin Hongu <dhongu@gmail.com>
2019-05-10 23:11:44 +0300

    Mici corectii la foi de parcurs

---
+++ alfa_fleet/README.rst
@@ -0,0 +1,65 @@
+==================================
+Extension for Fleet Application
+==================================
+.. image:: https://img.shields.io/badge/license-LGPL--3-blue.png
+   :target: http://www.gnu.org/licenses/lgpl-3.0-standalone.html
+   :alt: License: LGPL-3
+
+
+
+Acest modul este o extensie a aplicatiei de gestiune parc auto.
+
+Features:
+ * Campuri suplimentare in  detele de baza ale unui vehicul.
+        * indicativ
+        * sofer de rezerva
+        * categorie
+        * nivel rezervor
+        * capacitate rezervor
+        * consum mediu
+        * viteza medie
+        * carduri de alimentare
+ * Gestionare carduri de alimentare
+    * alocare carduri la masini si la alimentari
+ * Gestionare trasee
+        * gestionare locatii
+        * gestionare distante intre locatii si durate de deplasare
+ * Gestionare foi de parcurs
+        * trasee aferente unei foi de parcusrs
+        * carburant
+ * Calculare automata a nivelului de carburant din rezervor
+        * caclul carburant in foaia de parcurs dupa alocarea alimentarilor
+
+Nota
+Campul data din fleet trebuie modificat in datatime
+
+
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/dhongu/deltatech/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smash it by providing detailed and welcomed feedback.
+
+Credits
+=======
+
+
+Contributors
+------------
+
+* Dorin Hongu <dhongu@gmail.com>
+
+
+Maintainer
+----------
+
+.. image:: https://apps.odoo.com/apps/modules/12.0/deltatech/logo-terrabit.png
+   :alt: Terrabit
+   :target: https://terrabit.ro
+
+This module is maintained by the Terrabit.
+
+ 
--- alfa_fleet/report/fleet_map_sheet.py
+++ alfa_fleet/report/fleet_map_sheet.py
@@ -1,49 +1,34 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright (c) 2015 Deltatech All Rights Reserved
-#                    Dorin Hongu <dhongu(@)gmail(.)com       
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# ©  2015-2019 Deltatech
+#              Dorin Hongu <dhongu(@)gmail(.)com
+# See README.rst file on addons root folder for license details
+
 
 
 import time
-from odoo.report import report_sxw
+
 from odoo.osv import osv
 
  
 
 
 
-class map_sheet(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context):
-        super(map_sheet, self).__init__(cr, uid, name, context=context)
-        self.localcontext.update({
-            'time': time,
-        })
+# class map_sheet(report_sxw.rml_parse):
+#     def __init__(self, cr, uid, name, context):
+#         super(map_sheet, self).__init__(cr, uid, name, context=context)
+#         self.localcontext.update({
+#             'time': time,
+#         })
 
 
 
-        
-    
-class report_map_sheet(osv.AbstractModel):
-    _name = 'report.alfa_fleet.report_map_sheet'
-    _inherit = 'report.abstract_report'
-    _template = 'alfa_fleet.report_map_sheet'
-    _wrapped_report_class = map_sheet
+#
+#
+# class report_map_sheet(osv.AbstractModel):
+#     _name = 'report.alfa_fleet.report_map_sheet'
+#     _inherit = 'report.abstract_report'
+#     _template = 'alfa_fleet.report_map_sheet'
+#     _wrapped_report_class = map_sheet
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
